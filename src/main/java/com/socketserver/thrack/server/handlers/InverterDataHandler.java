c74f3f05ee85b01dad6b7129c0225aafa9794e7e
happyjun190 <happyjun190@gmail.com>
2017-03-18 23:55:33 +0800

    dtu验证通过后，对改dtu下所有逆变器进行基本处理

--- src/main/java/com/socketserver/thrack/server/handlers/InverterDataHandler.java
+++ src/main/java/com/socketserver/thrack/server/handlers/InverterDataHandler.java
@@ -2,6 +2,7 @@ package com.socketserver.thrack.server.handlers;
 
 import com.socketserver.thrack.commons.CodeUtils;
 import com.socketserver.thrack.server.client.Client;
+import com.socketserver.thrack.server.client.ClientInverterStats;
 import com.socketserver.thrack.server.client.ClientMap;
 import com.socketserver.thrack.server.client.Constants;
 import io.netty.channel.ChannelHandlerContext;
@@ -11,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by wushenjun on 2017/3/17.
@@ -29,6 +32,38 @@ public class InverterDataHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        byte[] message = (byte[])msg;
+        if(message.length<=5) {//读响应一般5+2*N, 写响应一般8字节，读写响应出错5字节
+            return;
+        }
+        String messageToStr = CodeUtils.getHexString(message);
+        Client client = ClientMap.getClient(ctx.channel());
+        logger.info("client:{}, and the response is :{}", client, messageToStr);
+
+        Map<String, ClientInverterStats> inverterStatsMap = client.getInverterStatsMap();
+        //逆变器地址
+        String inverterDeviceAddr = this.getInverterDeviceAddr(message);
+        //逆变器信息
+        ClientInverterStats clientInverterStats = inverterStatsMap.get(inverterDeviceAddr);
+        if(inverterStatsMap==null||inverterStatsMap.isEmpty()||clientInverterStats==null) {//null and empty判断
+            logger.info("dtu逆变器设备不存在,请于管理后台配置逆变器设备信息并重启设备！");
+            return;
+        }
+
+        //数据处理
+
+
+    }
+
+
+    /**
+     * 获取逆变器地址
+     * @param message
+     * @return
+     */
+    private String getInverterDeviceAddr(byte[] message) {
+        byte[] addrBytes = {message[0], message[1]};
+        return CodeUtils.getHexStringNoBlank(addrBytes);
     }
 
 }
