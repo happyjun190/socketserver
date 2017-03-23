b93e1b7a78f523749e5618eec47652d223610905
happyjun190 <happyjun190@gmail.com>
2017-03-24 00:00:22 +0800

    处理展示需要用到的基本数据，beta1

--- src/main/java/com/socketserver/thrack/service/impl/DataDealService.java
+++ src/main/java/com/socketserver/thrack/service/impl/DataDealService.java
@@ -24,47 +24,82 @@ public class DataDealService implements IDataDealService {
     @Override
     public void dataDealOfAddr1600(byte[] message, ClientInverterStats clientInverterStats) {
         byte[] dataBytes = this.getDataBytes(message);
-        //峰值功率 KW
-        //byte[] peakPowerBytes = {dataBytes[0], dataBytes[1], dataBytes[2], dataBytes[3]};
-        byte[] peakPowerBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 0, 4);
-        BigDecimal peakPower = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(peakPowerBytes, 1000);
+        //今日峰值功率 KW
+        byte[] todayPeakPowerBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 0, 4);
+        BigDecimal todayPeakPower = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(todayPeakPowerBytes, 1000);
+
+        //历史峰值功率 KW
+        byte[] historyPeakPowerBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 24, 4);
+        BigDecimal historyPeakPower = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(historyPeakPowerBytes, 1000);
+
+        logger.info("转换今日峰值功率和历史峰值功率，todayPeakPower：{}KW，historyPeakPower：{}KW", todayPeakPower, historyPeakPower);
+
         //BigDecimal peakPower =
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr1616(byte[] message, ClientInverterStats clientInverterStats) {
+        byte[] dataBytes = this.getDataBytes(message);
+        //日发电量(KWh)
+        byte[] todayGeneratingCapacityBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 0, 4);
+        BigDecimal todayGeneratingCapacity = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(todayGeneratingCapacityBytes, 100);
+
+        //日省钱
+        byte[] todaySaveMoneyBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 4, 4);
+        BigDecimal todaySaveMoney = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(todaySaveMoneyBytes, 100);
+
+        //日减排CO2(Kg)
+        byte[] todayCO2EmissionReductionBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 8, 4);
+        BigDecimal todayCO2EmissionReduction = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(todayCO2EmissionReductionBytes, 100);
+
+        logger.info("转换今日发电量(KWh)，日省钱和日减排CO2(Kg)，todayGeneratingCapacity：{}KWh，todaySaveMoney：{}元，todayCO2EmissionReduction：{}Kg",
+                                                                                        todayGeneratingCapacity, todaySaveMoney,todayCO2EmissionReduction);
 
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr1652(byte[] message, ClientInverterStats clientInverterStats) {
+        byte[] dataBytes = this.getDataBytes(message);
+        //累计发电量(KWh)
+        byte[] totalGeneratingCapacityBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 0, 4);
+        BigDecimal totalGeneratingCapacity = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(totalGeneratingCapacityBytes, 100);
 
+        //累计省钱
+        byte[] totalSaveMoneyBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 4, 4);
+        BigDecimal totalSaveMoney = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(totalSaveMoneyBytes, 100);
+
+        //累计减排CO2(Kg)
+        byte[] totalCO2EmissionReductionBytes = DataTransformUtils.getBytesArrFromOffsetAndLength(dataBytes, 8, 4);
+        BigDecimal totalCO2EmissionReduction = DataTransformUtils.tranfrom4ByteAndMulToUnsignedRealValue(totalCO2EmissionReductionBytes, 100);
+
+        logger.info("转换累计发电量(KWh)，累计省钱和累计减排CO2(Kg)，totalGeneratingCapacity：{}KWh，totalSaveMoney：{}元，totalCO2EmissionReduction：{}Kg",
+                                                               totalGeneratingCapacity, totalSaveMoney, totalCO2EmissionReduction);
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr1670(byte[] message, ClientInverterStats clientInverterStats) {
-
+        byte[] dataBytes = this.getDataBytes(message);
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr168E(byte[] message, ClientInverterStats clientInverterStats) {
-
+        byte[] dataBytes = this.getDataBytes(message);
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr1690(byte[] message, ClientInverterStats clientInverterStats) {
-
+        byte[] dataBytes = this.getDataBytes(message);
     }
 
     @Transactional
     @Override
     public void dataDealOfAddr1800(byte[] message, ClientInverterStats clientInverterStats) {
-
+        byte[] dataBytes = this.getDataBytes(message);
     }
 
 
