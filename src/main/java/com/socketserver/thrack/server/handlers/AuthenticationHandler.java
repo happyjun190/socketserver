c24846f5a2b499583cd9af362ebb70efc4af432a
happyjun190 <happyjun190@gmail.com>
2017-03-30 19:53:43 +0800

    bug观察，还未处理请求数据刷新逆变器信息的bug

--- src/main/java/com/socketserver/thrack/server/handlers/AuthenticationHandler.java
+++ src/main/java/com/socketserver/thrack/server/handlers/AuthenticationHandler.java
@@ -121,6 +121,7 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
 		//TODO 需要修改if判断，现在测试不用crc16算法
 		if (!CodeUtils.checkCRC((byte[]) msg)) {
 		//if (CodeUtils.checkCRC((byte[]) msg)) {
+			//TODO 存在粘包问题
 			logger.info("此处为CRC认证失败，消息为:{}", CodeUtils.getHexString((byte[]) msg));
 			return;
 		} else {
